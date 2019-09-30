606275e1c3c79cc27f8da2244877f201b51c203a
dogunok <spam-system.s@yandex.ru>
2019-09-30 15:30:32 +0300

    добавил index.js

--- index.js
+++ index.js
@@ -16,5 +16,5 @@ var requireIndex = require("requireindex");
 
 
 // import all rules in lib/rules
-module.exports.rules = requireIndex(__dirname + "/rules");
+module.exports.rules = requireIndex(__dirname + "/lib/rules");
 
